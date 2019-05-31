bd60636bf9463146527178a21de69009eb1da0ab
Joe Johnson <johnson.joseph527@gmail.com>
2019-05-30 21:06:53 -0600

    All filters now work when searching

--- src/Containers/Filter/Filter.js
+++ src/Containers/Filter/Filter.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
 import './Filter.scss'
+import { getSearchedTrails } from '../../APICalls'
+import { getTrails } from '../../APICalls';
+import { setTrails } from '../../Actions';
+import { connect } from 'react-redux';
 
 class Filter extends Component {
   constructor() {
     super()
     this.state = {
-      search:''
+      search:'',
+      maxDistance: 20,
+      lat: 0,
+      lng: 0,
+      sort: 'quality',
+      minLength: 0,
+      minStars: 0,
+      maxResults: 20
     }
+  }
+
+  componentDidMount() {
+    this.getLocation()
+  }
+
+  getLocation = () => {
+    const success = this.setCurrentLocation
+    const error = this.errorLocating
+    const options = {timeout: 5000}
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error ,options )
+    } else {
+      console.log('not working');
+    }
+  }
+
+  setCurrentLocation = (pos) => {
+    this.setState({lat: pos.coords.latitude, lng: pos.coords.longitude}, () => {
+      getTrails(this.state)
+      .then(results => this.props.setTrails(results.trails))
+    })
+  }
+
+  errorLocating = () => {
+    console.log('FUCK')
   }
 
   handleChange = (e) => {
@@ -16,14 +53,50 @@ class Filter extends Component {
     })
   }
 
+  handleSearch = (e) => {
+    e.preventDefault();
+    getSearchedTrails(this.state.search)
+    .then(results => {
+      const { lat, lng } = results.results[0].geometry.location
+      this.setState({lat, lng})
+
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    getTrails(this.state)
+    .then(results => this.props.setTrails(results.trails))
+  }
+
   render() {
+    const { sort, maxDistance, minStars, search, minLength} = this.state
     return(
-      <div className='filter'>
-        <input name='search' value={this.state.search } onChange={this.handleChange} type='text'/>
-        <button>Search</button>
-      </div>
+      <form className='filter'>
+        <input name='maxDistance' onChange={this.handleChange} value={maxDistance} type='number'/>
+        <select value={sort} name='sort' onChange={this.handleChange}>
+          <option value='quality'>Quality</option>
+          <option value='distance'>Distance</option>
+        </select>
+        <select value={minStars} name='minStars' onChange={this.handleChange}>
+          <option value={0}>All Ratings</option>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+        <input name='minLength' onChange={this.handleChange} value={minLength} type='number'/>
+        <input name='search' value={search} onChange={this.handleChange} type='text'/>
+        <button onClick={(e) => this.handleSearch(e)}>Update Location</button>
+        <button onClick={(e) => this.handleSubmit(e)}>Update Results</button>
+      </form>
     )
   }
 }
 
-export default Filter;
+export const mapDispatchToProps = dispatch => ({
+  setTrails: trails => dispatch(setTrails(trails)),
+});
+
+export default connect(null, mapDispatchToProps)(Filter);
--- src/Containers/Results/Results.js
+++ src/Containers/Results/Results.js
@@ -1,45 +1,10 @@
 import React, { Component } from 'react';
 import './Results.scss'
-import { getLocalTrails } from '../../APICalls';
-import { setTrails } from '../../Actions';
 import { connect } from 'react-redux';
 import Trail from '../Trail/Trail'
 
 
 class Results extends Component {
-  constructor() {
-    super()
-    this.state = {
-      lat: 0,
-      long: 0
-    }
-  }
-
-  componentDidMount() {
-    this.getLocation()
-  }
-
-  getLocation = () => {
-    const success = this.located
-    const error = this.errorLocating
-    const options = {timeout: 5000}
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, error ,options )
-    } else {
-      console.log('not working');
-    }
-  }
-
-  located = (pos) => {
-    this.setState({lat: pos.coords.latitude, long: pos.coords.longitude}, () => {
-      getLocalTrails(this.state)
-      .then(results => this.props.setTrails(results.trails))
-    })
-  }
-
-  errorLocating = () => {
-    console.log('FUCK')
-  }
 
   displayTrails = () => {
     if(this.props.trails) {
@@ -62,8 +27,4 @@ export const mapStateToProps = state => ({
   trails: state.trails
 })
 
-export const mapDispatchToProps = dispatch => ({
-  setTrails: trails => dispatch(setTrails(trails)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Results);
+export default connect(mapStateToProps)(Results);
