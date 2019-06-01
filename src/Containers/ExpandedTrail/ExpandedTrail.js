fd657d7fa125106a6bf8fac690296b19395a533c
Joe Johnson <johnson.joseph527@gmail.com>
2019-06-01 01:40:41 -0600

    Data is now avialble to be displayed on the expanded card

--- src/Containers/ExpandedTrail/ExpandedTrail.js
+++ src/Containers/ExpandedTrail/ExpandedTrail.js
@@ -2,19 +2,38 @@ import React, { Component } from 'react';
 // import Ratings from 'react-ratings-declarative';
 // import noPhoto from '../../Media/no-photo.png';
 import './ExpandedTrail.scss'
+import { getMyHikes} from '../../APICalls'
 
 class ExpandedTrail extends Component {
+  constructor() {
+    super()
+   this.state = {trail: {}}
+  }
+
+  componentDidMount() {
+    this.getAllTrailIfno(this.props.match.params.id)
+  }
 
   handleClick = (e) => {
     window.history.back()
+  }
+
+  getAllTrailIfno = (id) => {
+    getMyHikes(id)
+    .then(result => this.setState({trail: result.trails[0]}))
   }  
 
   render() {
-    // const { name, difficulty, stars, starVotes, length, imgMedium, expandTrail, hiked, hikeLater } = this.props
+    const { name, difficulty, stars, starVotes, length, imgMedium, expandTrail } = this.state.trail
     return (
       <div className='trail-overlay'>
         <div className='expanded-trail'>
-          <p>This is a trail</p>
+          <p>{name}</p>
+          <p>{stars}</p>
+          <p>{difficulty}</p>
+          <p>{starVotes}</p>
+          <p>{length}</p>
+          <img src={imgMedium}/>
           <button onClick={() => this.handleClick()}>x</button>
         </div>
       </div>
