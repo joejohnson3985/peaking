3e40987bf0c4b5fd16a9eb2f8e6af95876d6d3a2
Joe Johnson <johnson.joseph527@gmail.com>
2019-06-06 20:54:59 -0600

    Updated snapshot and removed unused var

--- src/Containers/Stats/Stats.js
+++ src/Containers/Stats/Stats.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './Stats.scss'
-import { NavLink } from 'react-router-dom';
 
 
 class Stats extends Component {
